d2fb622750823c15dc4f0bd7cba7ccc230121ef1
eogieglo <eric.ogieglo@gmail.com>
2020-08-26 13:46:31 -0300

    Choosing When to Filter

---
+++ 12_ChoosingWhentoFilter.js
@@ -0,0 +1,19 @@
+/* Nombre con numeros los archivos Js para que se corresponda con los numeros de videos
+
+--- EJECUTAR node 12_ChoosingWhentoFilter.js para ver el resultado en la consola */
+
+let post = { id: 4, title: 'New Post' };
+let comments = [
+    { postId: 4, content: 'awesome post' },
+    { postId: 3, content: 'it was ok' },
+    { postId: 4, content: 'neat' }
+];
+
+function commentsForPost(post, comments) {
+    return comments.filter(function(comment) {
+        return comment.postId === post.id;
+    });
+}
+console.log('Los post y comentarios son:');
+console.log('----------------------------');
+console.log(commentsForPost(post, comments));
