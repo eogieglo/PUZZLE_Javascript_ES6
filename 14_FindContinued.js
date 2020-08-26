cf609c89bac2119f6ea813f3adb617ad3800691b
eogieglo <eric.ogieglo@gmail.com>
2020-08-26 14:32:41 -0300

    Find Continued

---
+++ 14_FindContinued.js
@@ -0,0 +1,41 @@
+/* Nombre con numeros los archivos Js para que se corresponda con los numeros de videos
+
+--- EJECUTAR node 14_FindContinued.js para ver el resultado en la consola */
+
+function Car(model){
+    this.model = model; 
+}
+
+let cars = [
+    new Car('Buick'),
+    new Car('Camaro'),
+    new Car('Focus')
+];
+
+let filterFocus = cars.find(function(car){
+    return car.model === 'Focus';
+});
+
+console.log('El resultado del filtro es: ');
+console.log('--------------------------- ');
+console.log(filterFocus);
+console.log('');
+
+//En el video Borra el codigo y empieza con uno nuevo
+
+let posts = [
+    { id: 1, title: 'New Post' },
+    { id: 2, title: 'Old Post' }
+];
+
+let comment = { postId: 1, content: 'Great Post'};
+
+function postForComment(posts, comment) {
+    return posts.find(function(post){
+        return post.id === comment.postId;
+    });
+}
+
+console.log('El post comentario es: ');
+console.log('-----------------');
+console.log(postForComment(posts, comment));
