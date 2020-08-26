881ffa56bc585e14fb543dd071c25bd54fed93a3
eogieglo <eric.ogieglo@gmail.com>
2020-08-26 10:16:52 -0300

    forEach Continued

---
+++ 5_forEach_Continued.js
@@ -0,0 +1,21 @@
+/* Nombre con numeros los archivos Js para que se corresponda con los numeros de videos
+
+--- EJECUTAR node 5_ForEach_Continued.js para ver el resultado en la consola */
+
+//  Create an array of numbers
+let numbers = [1,2,3,4,5];
+
+//  Create a variable to hold the sum
+let sum = 0;
+
+function adder(number){ 
+    sum += number;
+}
+
+//  Loop over the array, incrementing the sum variable
+numbers.forEach(adder);
+
+// Print the sum variable
+console.log('El resultado de la suma es: ');
+console.log('---------------------------');
+console.log(sum);
