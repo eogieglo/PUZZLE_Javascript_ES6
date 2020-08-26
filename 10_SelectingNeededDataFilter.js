6a1a94b6d78741bb6368de55421d43c59446cea5
eogieglo <eric.ogieglo@gmail.com>
2020-08-26 11:05:44 -0300

    Selecting Needed Data Filter

---
+++ 10_SelectingNeededDataFilter.js
@@ -0,0 +1,23 @@
+/* Nombre con numeros los archivos Js para que se corresponda con los numeros de videos
+
+--- EJECUTAR node 10_SelectingNeededDataFilter para ver el resultado en la consola */
+
+let products = [
+    { name: 'cucumber', type: 'vegetable' },
+    { name: 'banana', type: 'fruit' },
+    { name: 'celery', type: 'vegetable' },
+    { name: 'orange', type: 'fruit' }
+];
+
+let filteredProducts = [];
+
+for (let i = 0; i < products.length; i++) {
+    if(products[i].type  === 'fruit'){
+        filteredProducts.push(products[i]);
+    }
+}
+
+console.log(filteredProducts.length);
+
+let filter2 = JSON.stringify(filteredProducts)
+console.log(filter2);
