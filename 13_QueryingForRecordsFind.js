7e4e20615b7cc2dd2840e396a3d9d9e6a8fcfd4d
eogieglo <eric.ogieglo@gmail.com>
2020-08-26 13:57:12 -0300

    QueryingForRecordsFind

---
+++ 13_QueryingForRecordsFind.js
@@ -0,0 +1,32 @@
+/* Nombre con numeros los archivos Js para que se corresponda con los numeros de videos
+
+--- EJECUTAR node 13_QueryingForRecordsFind.js para ver el resultado en la consola */
+
+let users = [
+    { name: 'Jill'},
+    { name: 'Alex'},
+    { name: 'Bill'}
+];
+
+let user;
+
+for ( let i = 0; i < users.length; i++){
+    if (users[i].name === 'Alex') {
+        user = users[i];
+        break;
+    }
+}
+
+console.log('El user es: ');
+console.log('-----------');
+console.log(user);
+
+console.log('');
+
+let find1 = users.find(function(user){
+    return user.name === 'Alex'
+});
+
+console.log('El find metodo nos da el usuario:');
+console.log('-------------------');
+console.log(find1);
