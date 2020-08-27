60a251d82665850ceba406b089c9fbe80282fb69
eogieglo <eric.ogieglo@gmail.com>
2020-08-27 00:36:29 -0300

    Advanced Arrow Function

---
+++ 28_AdvancedArrowFunction.js
@@ -0,0 +1,17 @@
+/* Nombre con numeros los archivos Js para que se corresponda con los numeros de videos
+
+--- EJECUTAR node 28_AdvancedArrowFunctions.js para ver el resultado en la consola */
+
+const double = (number) => {
+    return 2 * number;
+};
+
+console.log(double(4));
+
+//Otro ejemplo en el mismo video
+
+const numbers = [1,2,3];
+
+let numbersMap = numbers.map(number => 2 * number);
+
+console.log(numbersMap);
