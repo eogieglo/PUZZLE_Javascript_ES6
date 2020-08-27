7ca0b35744b419d4ffaa72c5319fe6d4012b5ea0
eogieglo <eric.ogieglo@gmail.com>
2020-08-26 23:36:43 -0300

    Next Interview Reduce

---
+++ 22_NextInterviewReduce.js
@@ -0,0 +1,13 @@
+/* Nombre con numeros los archivos Js para que se corresponda con los numeros de videos
+
+--- EJECUTAR node 22_NextInterviewReduce para ver el resultado en la consola */
+
+function balancedParens(string){
+    return string.split("").reduce(function(previous, char){
+        if (char === '(') { return ++previous; }
+        if (char === ')') { return --previous; }
+        return previous;
+    }, [0]);
+}
+
+console.log(balancedParens(')('));
