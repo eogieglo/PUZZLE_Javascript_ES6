31ce6dac06f323f9e7d2d4637924f188b3cb7238
eogieglo <eric.ogieglo@gmail.com>
2020-08-27 00:43:13 -0300

    When Use Arrow Function

---
+++ 29_WhenUseArrowFunction.js
@@ -0,0 +1,15 @@
+/* Nombre con numeros los archivos Js para que se corresponda con los numeros de videos
+
+--- EJECUTAR node 29_WhenUseArrowFunctions.js para ver el resultado en la consola */
+
+const team = {
+    members: ['Jane','Bill'],
+    teamName: 'Super Squad',
+    teamSummary: function() {
+        return this.members.map(function(member){
+            return `${member} is on team ${this.teamName}`; 
+        });
+    }
+}
+
+console.log(team.teamSummary());
