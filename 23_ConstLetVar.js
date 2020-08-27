1b2cb144d365f3af0b09e45af629896deebdd258
eogieglo <eric.ogieglo@gmail.com>
2020-08-26 23:50:54 -0300

    Const Let Vars

---
+++ 23_ConstLetVar.js
@@ -0,0 +1,38 @@
+/* Nombre con numeros los archivos Js para que se corresponda con los numeros de videos
+
+--- EJECUTAR node 23_ConstLetVar.js para ver el resultado en la consola */
+ 
+//Ejemplo declaracion de variable
+/*
+var name = 'Jane';
+var title = 'Software Engineer';
+var hourlyWage = 40;
+
+console.log('');
+console.log('Variables a la antigua: ');
+console.log(name);
+console.log(title);
+console.log(hourlyWage);*/
+
+// ES6
+
+const $name = 'Jane'
+let $title = 'Software Engineer';
+let $hourlyWage = 40;
+
+console.log('');
+console.log('Variables ES6: ');
+console.log($name);
+console.log($title);
+console.log($hourlyWage);
+
+// some time later..
+
+$title = 'Senior Software Engineer';
+$hourlyWage = 45;
+
+console.log('');
+console.log('Variables modificadas ES6: ');
+console.log($name);
+console.log($title);
+console.log($hourlyWage);
