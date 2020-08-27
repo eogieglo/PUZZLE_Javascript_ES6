3b9413317037c2beef3a0fe8a093571059bcbdfa
eogieglo <eric.ogieglo@gmail.com>
2020-08-27 00:30:53 -0300

    Fat Arrow Function

---
+++ 27_FatArrowFunctions.js
@@ -0,0 +1,16 @@
+/* Nombre con numeros los archivos Js para que se corresponda con los numeros de videos
+
+--- EJECUTAR node 27_FatArrowFunctions.js para ver el resultado en la consola */
+
+const add = (a,b) => {
+    return a + b;
+};
+
+console.log(add(1,2));
+
+// Otra forma de escribir Arrow Function
+
+
+const add2 = (a,b) =>  a+b;
+
+console.log(add2(1,2));
